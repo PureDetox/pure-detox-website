ebbc2ffe918f92c30591d66c2f4d8cfc49b8e24a
PureDetox <ridwan@puredetox.app>
2025-10-03 20:50:49 +0530

    Add files via upload

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,20 +4,20 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBs8PR2dnhNnK89JYzKRiCtXmvyKhSbyY8",
+  apiKey: "AIzaSyDnxbucjjaoR8FlUIpz7Te72JSNIZlUpYE",
   authDomain: "puredetox-d0fb5.firebaseapp.com",
   databaseURL: "https://puredetox-d0fb5-default-rtdb.firebaseio.com",
   projectId: "puredetox-d0fb5",
   storageBucket: "puredetox-d0fb5.firebasestorage.app",
   messagingSenderId: "1074058101974",
-  appId: "1:1074058101974:web:29dbec7d514608e6317577",
-  measurementId: "G-Z7S3E48LKL"
+  appId: "1:1074058101974:web:29dbec7d514608e6317577"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // Initialize Cloud Firestore and get a reference to the service
-export const db = getFirestore(app);
+// Use named Firestore database instead of default
+export const db = getFirestore(app, 'puredetox-d0fb5');
 
 export default app;
